76ec4dd515f13674740c1c5d002eefb16b025cd0
Vanchesky <ivankayukoff@gmail.com>
2019-09-27 22:52:58 +0300

    implement first part of the 2nd lab

--- lab2/executable-list.sh
+++ lab2/executable-list.sh
@@ -2,14 +2,6 @@
 
 read -r -a groups <<< "$(groups)"
 
-# DEBUG MODE ON
-for item in "${groups[@]}"
-do
-  echo "$item"
-done
-echo
-echo "Number of elements: ${#groups[@]}"
-
 # Checks if the element is present in the array
 # Returns 0 if arr contains the argument, otherwise - 1
 # USAGE:
@@ -28,16 +20,53 @@ array_contains () {
   return $in
 }
 
+# Checks if the file can be executed by $USER
+# Argument: $1 - 'ls -l' output, accepts only 1 line
+# Returns 0 if the file can be executed, otherwise - 1
+# USAGE:
+# user_executable "$ll_output" && echo yes || echo no
+user_executable() {
+  local ll_line=$1
+  # Check if the file executable by user
+  if [[ $USER == $(echo $ll_line | awk '{print $3}') ]]; then
+    if [[ $ll_line =~ ^-..x ]]; then
+      return 0
+    else
+      return 1
+    fi
+  fi
+
+  # Check if the file executable by one of the user's group
+  if array_contains groups "$(echo $ll_line | awk '{print $4}')" ; then
+    if [[ $ll_line =~ ^-.{5}x ]]; then
+      return 0
+    else
+      return 1
+    fi
+  fi
+
+  # Check if the file executable by others
+  if [[ $ll_line =~ ^-.{8}x ]]; then
+    return 0
+  fi
+
+  return 1
+}
+
 IFS=':'
 read -r -a paths <<< "$PATH"
+IFS=$'\n'
 
-# DEBUG MODE ON
+files=()
 for item in "${paths[@]}"
 do
-  echo "$item"
+  files+=($(ls -l $item | grep -v '^total'))
 done
-echo
-echo "Number of elements: ${#paths[@]}"
 
-#TODO print list of binaries
+for item in "${files[@]}"
+do
+  if user_executable $item ; then
+    echo $item
+  fi
+done
 
